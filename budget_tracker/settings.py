c4122f9385194776fbd4bfc5ce4a701c99960958
rahul-312 <rahulsid49@gmail.com>
2025-05-13 11:06:52 +0530

    chnaged dir

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-hiq6*eitd*#^-6z$4-+kx3rlfzi1hny0m2h^)6wx-9hlz&whmg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'users.User'
 
